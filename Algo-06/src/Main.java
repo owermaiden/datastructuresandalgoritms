c35e0e2e5505f37f9d17a7ff39e0ab4bc9a217ec
owermaiden <ower.maiden@cybertekschool.com>
2021-11-03 10:02:03 +0300

    Algo-6

--- Algo-06/src/Main.java
+++ Algo-06/src/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         int targetSum = 0;
         int[] draftArray = { 12, 3, 1,2,-6,5,-8,6};
+        int[] edgeCases = {-4,-3,-2,-1,3,4,5,6};
 
         System.out.println(Arrays.deepToString(threeNumberSum(draftArray, targetSum)));
     }
@@ -13,20 +14,22 @@ public class Main {
         int[] orderedArray = Arrays.stream(draftArray).sorted().toArray();
         int arrayLength = draftArray.length;
         int x = 0; int y = 0;
-        int[][] resultArray = new int[arrayLength][3];
+        int[][] resultArray = new int[arrayLength / 2][3];
 
         for (int i = 0; i < arrayLength - 4; i++){
-            for (int j = i; j < arrayLength -1; j++){
-                for (int k = j; k < arrayLength - 1; k++){
+            for (int j = i + 1; j < arrayLength -1; j++){
+                for (int k = j + 1; k < arrayLength - 1; k++){
                     if ((orderedArray[i] + orderedArray[j] + orderedArray[k]) == targetSum){
                         resultArray [x][y++] = orderedArray[i];
                         resultArray [x][y++] = orderedArray[j];
                         resultArray [x++][y] = orderedArray[k];
                         y = 0;
+                        break;
                     }
                 }
             }
         }
+
        return resultArray;
     }
 }

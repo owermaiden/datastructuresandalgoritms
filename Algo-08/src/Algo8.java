609008ce05bf0f64421abb739d9ccc44906bc351
owermaiden <ower.maiden@cybertekschool.com>
2021-11-03 16:23:25 +0300

    Algo-8

---
+++ Algo-08/src/Algo8.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+import java.util.Queue;
+
+public class Algo8 {
+    public static void main(String[] args) {
+
+        int[][] intervals = {{1,2},{3,5},{4,7},{6,8},{9,10}};
+        mergeOverlapping(intervals);
+        System.out.println(Arrays.deepToString(mergeOverlapping(intervals)));
+    }
+
+    public static int[][] mergeOverlapping(int[][] firstArray){
+
+        int[] flatArray = Arrays.stream(firstArray)
+                .flatMapToInt(Arrays::stream)
+                .toArray();
+
+        int[] removedOverlap = new int[10];
+        int i = 0;int j = 0;
+        do {
+            removedOverlap[i++] = flatArray[j++];
+            if (j == flatArray.length - 2) {
+                removedOverlap[i++] = flatArray[j++];
+                removedOverlap[i] = flatArray[j];
+                break;
+            }
+            while (flatArray[j] > flatArray[j + 1]){
+                j+=2;
+            }
+        } while (j < flatArray.length - 2);
+
+        int[][] newArray = new int[firstArray.length][2];
+        int count = 0;
+        for (int x = 0; x < 5; x++){
+            for (int y = 0; y < 2; y++){
+                newArray[x][y] = removedOverlap[count++];
+            }
+        }
+        return newArray;
+    }
+}

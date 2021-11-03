b939fab57564390d4f32a94a67bf6cfb352bfbd5
owermaiden <ower.maiden@cybertekschool.com>
2021-11-04 00:02:31 +0300

    Algo-4

---
+++ Algo-04/src/Algo4.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Algo4 {
+    public static void main(String[] args) {
+        int[] givenArray = {5,1,4,2};
+        System.out.println(productsOfOthers(givenArray));
+
+
+    }
+
+    public static List<Integer> productsOfOthers(int[] givenArray) {
+        var resultArray = new ArrayList<Integer>();
+
+        int pointer = 0;
+
+        while (pointer < givenArray.length) {
+            int finalPointer = pointer;
+            int carry = Arrays.stream(givenArray).filter(n -> n != givenArray[finalPointer]).reduce(1, (a, b) -> a * b);
+            resultArray.add(carry);
+            pointer++;
+        }
+        return resultArray;
+    }
+}

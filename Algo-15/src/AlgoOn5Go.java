aa1715166b20354b01acb326f40bc17fb21c25b3
owermaiden <ower.maiden@cybertekschool.com>
2021-11-24 22:29:20 +0300

    SubArraySort

---
+++ Algo-15/src/AlgoOn5Go.java
@@ -0,0 +1,56 @@
+import java.util.Arrays;
+
+
+public class AlgoOn5Go {
+    public static void main(String[]args){
+        int[] givenArray = {1,2,4,7,10,11,7,12,6,7,16,18,19};
+        System.out.println(Arrays.toString(findTheUnsortedPart(givenArray)));
+    }
+
+    static int[] findTheUnsortedPart(int[] arr){
+        int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
+        int[] result = {-1,-1};
+
+        for(int i = 0;i< arr.length;i++){
+            int j = arr.length-1-i;
+
+            if(i != arr.length-1 && arr[i+1]<arr[i]) max = Math.max(arr[i],max );
+            if(j != 0 && arr[j-1]>arr[j]) min = Math.min(arr[j],min);
+
+            if( arr[i]<=max) result[1] = i;
+            if( arr[j]>=min) result[0] = j;
+        }
+        return result;
+    }
+
+    static int[] findTheUnsortedPart2(int[] arr){
+        int i = 0,j = arr.length-1, min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
+        int[] result = {-1,-1};
+
+        while(i< arr.length){
+
+            if(i != arr.length-1 && arr[i+1]<arr[i]) max = Math.max(arr[i],max );
+            if(j != 0 && arr[j-1]>arr[j]) min = Math.min(arr[j],min);
+
+            if( arr[i++]<=max) result[1] = i-1;
+            if( arr[j--]>=min) result[0] = j+1;
+        }
+        return result;
+    }
+
+    static int[] findTheUnsortedPartWithSort(int[] arr){
+        int[] sorted = Arrays.copyOf(arr,arr.length);
+        int[] result = {-1,-1};
+        int left=-1, right= arr.length;
+        Arrays.sort(sorted);
+
+        for(int i=0;i< arr.length;i++){
+
+            int j= arr.length-1-i;
+            if(left == -1 && sorted[i]!=arr[i]) result[0]=left=i;
+            if(right == arr.length && sorted[j]!=arr[j]) result[1]=right=j;
+        }
+
+        return result;
+    }
+}

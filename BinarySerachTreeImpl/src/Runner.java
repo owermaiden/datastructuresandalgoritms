c0ac826827da49742c279bf2f6b3ff7bcf145658
owermaiden <ower.maiden@cybertekschool.com>
2022-01-05 09:03:36 +0300

    AVLTree and Graph

--- BinarySerachTreeImpl/src/Runner.java
+++ BinarySerachTreeImpl/src/Runner.java
@@ -2,15 +2,12 @@ public class Runner {
     public static void main(String[] args) {
         Tree tree = new Tree();
         tree.insertTree(10);
-        tree.insertTree(5);
-        tree.insertTree(3);
-        tree.insertTree(7);
-        tree.insertTree(6);
+        tree.insertTree(8);
         tree.insertTree(12);
-        tree.insertTree(11);
-        tree.insertTree(20);
-        tree.traversePreOrder();
-        //tree.traverseInOrder();
-        System.out.println(tree);
+        tree.insertTree(9);
+        tree.insertTree(7);
+
+        System.out.println("tree.removeLeafs() = " + tree.getLeaves());
+
     }
 }

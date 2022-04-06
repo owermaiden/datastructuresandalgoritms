a9d341907271a09b29193a802fee0755e4950f80
owermaiden <ower.maiden@cybertekschool.com>
2022-04-06 12:28:29 +0300

    Array Product of Other Elements

--- Alumni-02/src/ArraySumOthers.java
+++ Alumni-02/src/ArraySumOthers.java
@@ -1,12 +1,14 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class ArraySumOthers {
     public static void main(String[] args) {
         int[] arr1 = { 1,2,3,4 };
-        System.out.println(Arrays.toString(calculateProducts(arr1)));
+        System.out.println(Arrays.toString(calculateProducts2(arr1)));
     }
 
-    private static int[] calculateProducts(int[] arr1) {
+    private static int[] calculateProducts(int[] arr1) {   // O (n2)
         int[] arr2 = new int[arr1.length];
 
         for (int i = 0; i < arr2.length; i++) {
@@ -16,5 +18,26 @@ public class ArraySumOthers {
                     .reduce(1, (curr, prod) -> prod * curr);
         }
         return arr2;
+    }
+
+    private static int[] calculateProducts2(int[] given) {   // O(n)
+        int[] result = new int[given.length];
+        for (int i = 0; i < given.length; i++) result[i] = products(given, i);
+        return result;
+    }
+
+    private static int products(int[] arr,int index){
+        List<Integer> leftProducts = new ArrayList<>();
+        List<Integer> rightProducts = new ArrayList<>();
+
+        for (int i = 0; i < index ; i++) {   // O(n)
+            leftProducts.add(arr[i]);
+        }
+
+        for (int i = index + 1; i < arr.length ; i++) {  // O(n)
+            rightProducts.add(arr[i]);
+        }
+
+        return leftProducts.stream().reduce(1,(a,b) -> a * b) * rightProducts.stream().reduce(1,(a,b) -> a * b);
     }
 }
